b60ec396839e800c0016edeec5d634e57a125499
Dmitry Vasilyev <qwitriy@gmail.com>
2019-08-04 01:09:41 +0700

    add redis and telegram

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module vk-to-telegram-reposter
 go 1.12
 
 require (
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/himidori/golang-vk-api v0.0.0-20190621070708-62241937677f
-	github.com/joho/godotenv v1.3.0
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/himidori/golang-vk-api"
-
 	"log"
 	"net/url"
 	"os"
+	"strconv"
 )
 
 func main() {
-	serviceToken := os.Getenv("SERVICE_TOKEN")
-	group := os.Getenv("VK_GROUP")
+	vkServiceToken := os.Getenv("VK_SERVICE_TOKEN")
+	vkGroup := os.Getenv("VK_GROUP")
+	tgBotToken := os.Getenv("TG_BOT_TOKEN")
+	tgChannelId := os.Getenv("TG_CHANNEL_ID")
+	redisAddress := os.Getenv("REDIS_URL")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
 
-	client, err := vkapi.NewVKClientWithToken(serviceToken, &vkapi.TokenOptions{
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,  // use default Addr
+		Password: redisPassword, // no password set
+	})
+
+	bot, err := tgbotapi.NewBotAPI(tgBotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client, err := vkapi.NewVKClientWithToken(vkServiceToken, &vkapi.TokenOptions{
 		ValidateOnStart: true,
 		ServiceToken:    true,
 	})
@@ -22,25 +39,60 @@ func main() {
 	}
 
 	params := url.Values{}
-	wall, _ := client.WallGet(group, 10, params)
+	wall, _ := client.WallGet(vkGroup, 10, params)
+
+	val, err := redisdb.Get("vk_last_post_id").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	vk_last_post_id, err := strconv.Atoi(val)
+	if err != nil {
+		panic(err)
+	}
 
 	for _, post := range wall.Posts {
-		if post.IsPinned == 1 || post.MarkedAsAd == 1 {
+		if post.IsPinned == 1 || post.MarkedAsAd == 1 || post.ID <= vk_last_post_id {
 			continue
 		}
+
+		text := post.Text
 
 		log.Println(post.Text)
 		log.Println(post.ID)
 
 		if post.Attachments != nil {
 			for _, attachment := range post.Attachments {
-				switch attachment.Type {
-				case "photo":
-					log.Println(attachment.Photo.Photo1280)
-				case "video":
-					log.Println(attachment.Video.FirstFrame800)
+				if attachment.Type == "photo" {
+					text += getInvisibleLink(" ", attachment.Photo.Photo604) // пустой символ чтобы прикрепить картинку в markdown без текста
+				} else if attachment.Type == "video" {
+					videoUrl := "https://vk.com/video" + string(attachment.Video.OwnerID) + "_" + string(attachment.Video.ID)
+					text += getInvisibleLink("\nВидео", videoUrl)
 				}
+				break
 			}
 		}
+		msg := tgbotapi.NewMessageToChannel(tgChannelId, post.Text)
+		vkPostUrl := "https://vk.com/wall" + vkGroup + "-" + string(post.ID)
+
+		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				tgbotapi.NewInlineKeyboardButtonURL("Пост", vkPostUrl)),
+		)
+
+		bot.Send(msg)
+		updateDb(redisdb, post.ID)
 	}
+}
+
+func updateDb(client *redis.Client, i int) {
+	err := client.Set("vk_last_post_id", i, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getInvisibleLink(text string, url string) string {
+	return "[" + text + "](" + url + ")"
 }
